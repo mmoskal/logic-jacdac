5d7201f205e93418c6720051932931ee2eb8301a
Michal Moskal <michal@moskal.me>
2022-03-17 14:57:58 -0700

    use specs for decoding

--- HighLevelAnalyzer.py
+++ HighLevelAnalyzer.py
@@ -3,6 +3,11 @@
 
 from saleae.analyzers import HighLevelAnalyzer, AnalyzerFrame, StringSetting, NumberSetting, ChoicesSetting
 from saleae.data import GraphTimeDelta
+import json
+import binascii
+import os
+
+service_specs = {}
 
 
 def fnv1(data):
@@ -10,6 +15,7 @@ def fnv1(data):
     for d in data:
         h = ((h * 0x1000193) & 0xffffffff) ^ d
     return h
+
 
 def hash(buf, bits):
     if (bits < 1):
@@ -20,61 +26,236 @@ def hash(buf, bits):
     else:
         return (h ^ (h >> bits)) & ((1 << bits) - 1)
 
+
 def idiv(a, b):
     return a // b
+
 
 def shortDeviceId(devid):
     h = hash(devid, 30)
     return (chr(0x41 + h % 26) +
-        chr(0x41 + idiv(h, 26) % 26) +
-        chr(0x30 + idiv(h, 26 * 26) % 10) +
-        chr(0x30 + idiv(h, 26 * 26 * 10) % 10))
+            chr(0x41 + idiv(h, 26) % 26) +
+            chr(0x30 + idiv(h, 26 * 26) % 10) +
+            chr(0x30 + idiv(h, 26 * 26 * 10) % 10))
+
+
+def hex(buf):
+    return binascii.hexlify(buf).decode()
+
+
+def u16(buf: bytes, off: int):
+    return buf[off] | (buf[off+1] << 8)
+
+
+def u32(buf: bytes, off: int):
+    return buf[off] | (buf[off+1] << 8) | (buf[off+2] << 16) | (buf[off+3] << 24)
+
+
+class Device:
+    def __init__(self, ann) -> None:
+        self.device_id = hex(ann[4:12])
+        self.short_id = shortDeviceId(ann[4:12])
+        self.update(ann)
+
+    def update(self, ann):
+        self.announce = ann[16:]
+
+    def num_services(self):
+        return len(self.announce) >> 2
+
+    def service_class_at(self, idx):
+        if idx == 0:
+            return 0
+        if idx < self.num_services():
+            return u32(self.announce, idx << 2)
+        return None
+
+    def service_spec_at(self, idx):
+        sclass = str(self.service_class_at(idx))
+        if sclass in service_specs:
+            return service_specs[sclass]
+        return None
+
+
+def lookup_pkt(spec, id, kinds):
+    if spec:
+        for pkt in spec['packets']:
+            if pkt['identifier'] == id and pkt['kind'] in kinds:
+                return pkt
+    return None
+
+
+def get_attrs(devs, pkt: bytes):
+    dev_id = hex(pkt[4:12])
+    dev: Device = None
+    size = pkt[12]
+    serv_idx = pkt[13]
+    serv_cmd = u16(pkt, 14)
+    is_ann = serv_idx == 0 and serv_cmd == 0
+    if dev_id in devs:
+        dev = devs[dev_id]
+        if is_ann:
+            dev.update(pkt)
+    else:
+        ann_pkt = pkt
+        if not is_ann:
+            ann_pkt = pkt[0:12] + bytes([0, 0, 0, 0])
+        dev = Device(ann_pkt)
+        devs[dev.device_id] = dev
+    spec = dev.service_spec_at(serv_idx)
+    is_broadcast = (pkt[3] & 0x04) != 0
+    if is_broadcast:
+        spec = service_specs[str(u32(pkt, 4))]
+    spec_name = spec['camelName'] if spec else "%d" % serv_idx
+    info = ""
+    needs_ack = (pkt[3] & 0x02) != 0
+    is_command = (pkt[3] & 0x01) != 0
+    is_report = not is_command
+    tp = "err"
+    attrs = {
+        'short_id': dev.short_id,
+        'service': "%d" % serv_idx,
+        'size': "%d" % size,
+        'cmd': "0x%04x" % serv_cmd,
+        'service_name': spec_name,
+    }
+    if is_broadcast:
+        attrs['short_id'] = "*BC*"
+    pkt_spec = None
+    id = None
+    if is_ann:
+        for i in range(1, dev.num_services()):
+            sp = dev.service_spec_at(i)
+            if sp:
+                info += "%s, " % sp['camelName']
+            else:
+                info += "0x%x, " % dev.service_class_at(i)
+        tp = "ann"
+    elif serv_idx == 0x3f:
+        tp = 'ack'
+        info = "ack:0x%x" % serv_cmd
+    elif serv_idx == 0x3e:
+        tp = 'pipe'
+        info = "port:%d cnt:%d%s%s" % (
+            serv_cmd >> 7,
+            serv_cmd & 0x1f,
+            " close" if serv_cmd & 0x20 else "",
+            " meta" if serv_cmd & 0x40 else ""
+        )
+    elif serv_idx >= 0x30:
+        tp = 'err'
+    elif is_report and serv_cmd & 0x8000:
+        tp = 'evt'
+        id = serv_cmd & 0xff
+        pkt_spec = lookup_pkt(spec, id, ["event"])
+    elif (serv_cmd >> 12) == 0x1 or (serv_cmd >> 12) == 0x2:
+        id = serv_cmd & 0xfff
+        pkt_spec = lookup_pkt(spec, id, ["rw", "ro", "const"])
+        if (serv_cmd >> 12) == 0x1:
+            tp = 'get'
+        else:
+            tp = 'set'
+        if is_report:
+            tp += 'v'
+    elif (serv_cmd >> 12) == 0x0:
+        id = serv_cmd & 0xfff
+        if is_command:
+            tp = 'cmd'
+            pkt_spec = lookup_pkt(spec, id, ["command"])
+        else:
+            tp = 'rep'
+            pkt_spec = lookup_pkt(spec, id, ["report"])
+    else:
+        tp = 'err'
+        info += "cmd:0x%x sz:%d" % (serv_cmd, size)
+    if id is not None:
+        if pkt_spec:
+            attrs['cmd_name'] = pkt_spec['name']
+        else:
+            attrs['cmd_name'] = "0x%x" % id
+    if needs_ack:
+        info = "[ack:0x%x] %s" % (u16(pkt, 0), info)
+    attrs['info'] = info
+    return (tp, attrs)
+
+
+def split_frame(frame: bytes):
+    if frame[2] - frame[12] < 4:
+        return [frame]
+    else:
+        ptr = 12
+        res = []
+        while ptr < 12 + frame[2]:
+            sz = frame[ptr] + 4
+            pktbytes = frame[0:12] + frame[ptr:ptr+sz]
+            res.append(pktbytes)
+            ptr += (sz + 3) & ~3
+        return res
+
+
+fmt = "{{type}} {{data.short_id}}/{{data.service_name}} {{data.cmd_name}} {{data.info}}"
+
 
 class Hla(HighLevelAnalyzer):
-    #my_string_setting = StringSetting(label='My String')
-    #my_number_setting = NumberSetting(label='My Number', min_value=0, max_value=100)
-    #my_choices_setting = ChoicesSetting(label='My Choice', ['A', 'B'])
+    # my_string_setting = StringSetting(label='My String')
+    # my_number_setting = NumberSetting(label='My Number', min_value=0, max_value=100)
+    # my_choices_setting = ChoicesSetting(label='My Choice', ['A', 'B'])
 
     # An optional list of types this analyzer produces, providing a way to customize the way frames are displayed in Logic 2.
     result_types = {
-        'pkt': {
-            'format': '{{data.info}}'
-        },
-        'ann': {
-            'format': '{{data.info}}'
-        },
+        'ann': {'format': fmt},
+        'ack': {'format': fmt},
+        'evt': {'format': fmt},
+        'pipe': {'format': fmt},
+        'get': {'format': fmt},
+        'getv': {'format': fmt},
+        'set': {'format': fmt},
+        'cmd': {'format': fmt},
+        'rep': {'format': fmt},
     }
 
     def __init__(self):
+        f = open(os.path.dirname(__file__) +
+                 "/services.json", encoding='utf-8')
+        arr = json.load(f)
+        f.close()
+        self.devices = {}
+        for serv in arr:
+            service_specs[str(serv['classIdentifier'])] = serv
         self.start_time = None
         self.end_time = None
         self.bytes = []
         pass
 
-    def flush(self):
-        if not self.end_time:
-            return
-        short_id = shortDeviceId(self.bytes[4:12])
-        size = self.bytes[12]
-        serv_num = self.bytes[13]
-        serv_cmd = self.bytes[14]+self.bytes[15]*256
-        info = "%s/%d: " % (short_id, serv_num)
-        tp = "pkt"
-        if serv_num == 0 and serv_cmd == 0:
-            info += "announce %d service(s)" % (size/4)
-            tp = "ann"
-        else:
-            info += "cmd:0x%x sz:%d" % (serv_cmd, size)
-        self.res = AnalyzerFrame(tp, self.start_time, self.end_time, {'info':info})
+    def clear(self):
         self.bytes = []
         self.end_time = None
         self.start_time = None
 
+    def flush(self):
+        if not self.end_time:
+            return
+        if len(self.bytes) < 16:
+            self.clear()
+            return
+
+        pkts = split_frame(bytes(self.bytes))
+        durr = self.end_time - self.start_time
+        idx = 0
+        lp = len(pkts)
+        delta = durr / lp
+        for pkt in pkts:
+            (tp, attrs) = get_attrs(self.devices, pkt)
+            self.res.append(AnalyzerFrame(
+                tp, self.start_time + (delta * idx), self.start_time + (delta * (idx+1)), attrs))
+            idx += 1
+        self.clear()
+
     def decode(self, frame: AnalyzerFrame):
-        self.res = None
+        self.res = []
         if not self.start_time:
             self.start_time = frame.start_time
-        if 'error' in frame.data or frame.start_time - self.start_time > GraphTimeDelta(millisecond=2):
+        if 'error' in frame.data or frame.start_time - self.start_time > GraphTimeDelta(millisecond=4):
             self.flush()
         if 'error' not in frame.data:
             self.bytes.append(ord(frame.data['data']))
